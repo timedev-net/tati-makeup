dbc3934a59f94118c75148594ffd204b253efc51
Daniel Frota <danielfrot@gmail.com>
2021-07-09 20:04:20 -0400

    sem variaveis de ambiente

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_KEY,
+  // process.env.NEXT_PUBLIC_SUPABASE_URL,
+  // process.env.NEXT_PUBLIC_SUPABASE_KEY,
+  "https://kmftqtuudtdencfhoyqk.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNTg2NTk3MiwiZXhwIjoxOTQxNDQxOTcyfQ.P-CraR1qgrQ-teHKPiYUyxHSO_x4dttslMaBs-KfDcI",
 )
